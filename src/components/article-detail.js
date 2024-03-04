5ac82897184cc09f829f57494420bac68af98e6f
noe.merle <noe.merle@epitech.eu>
2024-03-04 09:59:10 +0100

    [add]: fonction pour ajouter des articles aux paniers avec leurs infos

--- src/components/article-detail.js
+++ src/components/article-detail.js
@@ -22,3 +22,25 @@ document.addEventListener("DOMContentLoaded", function()
             console.log(error);
         });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtenir l'ID de l'article à partir de l'URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const articleId = urlParams.get('articleId');
+
+    document.getElementById('addToCartButton').addEventListener('click', function() {
+        axios.get('../../public/articleId.json')
+            .then(function(response) {
+                const article = response.data.articles.find(p => p.id == articleId);
+                if (article) {
+                    add_basket(article);
+                    alert('Article ajouté au panier.');
+                } else {
+                    alert('Article non trouvé.');
+                }
+            })
+            .catch(function(error) {
+                console.error('Erreur lors de la récupération des détails de l\'article :', error);
+            });
+    });
+});
