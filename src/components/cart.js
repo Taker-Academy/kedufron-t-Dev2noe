826548632634f75aa1e32b270ae9c79dc03c9ad0
noe.merle <noe.merle@epitech.eu>
2024-03-04 11:38:56 +0100

    [add]: cart.css, ajout de style pour chaque article du panier

--- src/components/cart.js
+++ src/components/cart.js
@@ -16,9 +16,9 @@ document.addEventListener("DOMContentLoaded", function()
             cartItem.querySelector('.cart-item-title').textContent = product.title;
             cartItem.querySelector('.cart-item-price').textContent = `${product.price}`;
             cartItem.querySelector('input[type=number]').value = product.quantity;
-            cartItem.querySelector('.cart-item-total').textContent = `${(product.price * product.quantity).toFixed(2)}`;
+            cartItem.querySelector('.cart-item-total').textContent = `${(parseInt(product.price, 10) * product.quantity).toFixed(2)}`;
 
-            // gestionnaires d'événements pour les boutons plus, moins, remove
+            // gestionnaires d'événements pour les boutons plus, moins, remove, quantité
             cartItem.querySelector('.remove-item').addEventListener('click', function() {
                 remove_from_basket(product.id);
                 loadCartItems();
@@ -31,9 +31,9 @@ document.addEventListener("DOMContentLoaded", function()
                 change_quantity(product.id, -1);
                 loadCartItems();
             });
-            const quantityInput = cartItem.querySelector('input[type=number]');
+            const quantityInput = cartItem.querySelector('input[type=number]'); // changer la quantité manuellement
             quantityInput.addEventListener('change', function() {
-                let newQuantity = parseInt(quantityInput.value, 10);
+                let newQuantity = parseInt(quantityInput.value, 10);  //transforme le string de la quantité en integer
                 if (isNaN(newQuantity) || newQuantity < 1) {
                     alert('La quantité ne peut pas être inférieure à 1.');
                     quantityInput.value = product.quantity;
@@ -58,7 +58,7 @@ function get_basket()
     let basket = localStorage.getItem("basket");
 
     if (basket == null) {
-        return []; // on créer un new cart
+        return []; // on créer un new cart (tableau vide)
     } else {
         return JSON.parse(basket); //on return le tableau chiffré en json
     }
